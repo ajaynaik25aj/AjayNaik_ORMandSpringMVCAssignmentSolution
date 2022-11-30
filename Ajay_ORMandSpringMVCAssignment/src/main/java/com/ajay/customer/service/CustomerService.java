2745f67b68f7f66c8d2b6fee7781519c6697d499
ajaynaik25aj <106276194+ajaynaik25aj@users.noreply.github.com>
2022-11-30 22:51:09 +0530

    Graded Assignment 3 - ORM & Spring MVC

---
+++ Ajay_ORMandSpringMVCAssignment/src/main/java/com/ajay/customer/service/CustomerService.java
@@ -0,0 +1,19 @@
+package com.ajay.customer.service;
+
+import java.util.List;
+
+import com.muneendra.customer.entity.Customer;
+
+public interface CustomerService {
+	
+	public List<Customer> findAll();
+
+	public void save(Customer theCustomer);
+
+	public Customer findById(int id);
+
+	public void deleteById(int id);
+		
+	
+
+}
